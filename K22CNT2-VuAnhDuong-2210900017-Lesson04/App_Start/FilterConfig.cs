b0fdfb18ce22751a5a88d15ae898847d0c14c00e
Vuanhduong25102004 <vuanhduong251020042@gmail.com>
2024-11-10 18:07:04 +0700

    Project2Lesson04

---
+++ K22CNT2-VuAnhDuong-2210900017-Lesson04/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace K22CNT2_VuAnhDuong_2210900017_
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ K22CNT2-VuAnhDuong-2210900017-Lesson04/Areas/AdminVAD/Controllers/VADDashboardController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace K22CNT2_VuAnhDuong_2210900017_.Areas.AdminVAD.Controllers
+{
+    public class VADDashboardController : Controller
+    {
+        // GET: AdminVAD/VADDashboard
+        public ActionResult Index()
+        {
+            if (Session["IsAdmin"] == null || !(bool)Session["IsAdmin"])
+            {
+                return RedirectToAction("Login", "Account", new { area = "AdminVAD" }); // Chuyển hướng về trang đăng nhập nếu không có session admin
+            }
+
+            return View();
+        }
+    }
+}
